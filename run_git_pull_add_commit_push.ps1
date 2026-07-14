730375574bf7d83a372b2027197229448538f88a
sinzy0925 <sinzy0925@gmail.com>
2026-07-14 18:13:32 +0900

    git pull add commit push 20260714 18:13

--- run_git_pull_add_commit_push.ps1
+++ run_git_pull_add_commit_push.ps1
@@ -1,8 +1,8 @@
 echo "---------------`n"
-echo "`ngit pull`n"
+echo "git pull`n"
 git pull
 echo "---------------`n"
-echo "`ngit add commit push`n"
+echo "git add commit push`n"
 git add .
 git commit -m "git pull add commit push $(Get-Date -Format 'yyyyMMdd HH:mm')"
 git push -u origin main
